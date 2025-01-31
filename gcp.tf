05fe1fcf83597c86dfc540c6f7fcc255247b35b0
tomaszstachera <tomasz.stachera.ext@siemens-energy.com>
2025-01-31 19:30:15 +0100

    Fixed startup scripts

--- gcp.tf
+++ gcp.tf
@@ -39,10 +39,12 @@ resource "google_compute_instance" "k3s_worker_gcp" {
   }
 
   metadata = {
-    ssh-keys       = "ubuntu:${tls_private_key.k3s_worker_gcp_key.public_key_openssh}"
-    startup_script = "#! /bin/bash\ncurl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_master.public_ip}:6443 K3S_TOKEN=${data.local_file.k3s_token.content} sh -s - --debug | tee /var/log/init.log"
+    ssh-keys = "ubuntu:${tls_private_key.k3s_worker_gcp_key.public_key_openssh}"
+    # I wasn't able to make GPP startup script to work
+    #startup_script = "curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_master.public_ip}:6443 K3S_TOKEN=${data.local_file.k3s_token.content} sh -s - --debug | tee /tmp/startup.log"
   }
 
+  # I wasn't able to make GPP startup script to work
   #metadata_startup_script = "curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_master.public_ip}:6443 K3S_TOKEN=${data.local_file.k3s_token.content} sh -s - --debug | tee /var/log/init.log"
 }
 
@@ -65,15 +67,26 @@ resource "null_resource" "gcp_wait_for_status" {
         echo "Waiting for instance to be running: Current status=$instance_status"
         sleep 10
       done
+      # Sometimes even though instance is running it won't accept SSH connection
+      sleep 30
     EOT
   }
 }
 
-resource "null_resource" "gcp_user_data_logs" {
-  depends_on = [null_resource.aws_user_data_logs, google_compute_instance.k3s_worker_gcp, null_resource.gcp_wait_for_status]
+# I wasn't able to make GPP startup script to work
+#resource "null_resource" "gcp_user_data_logs" {
+#  depends_on = [null_resource.aws_user_data_logs, null_resource.gcp_wait_for_status]
+#  provisioner "local-exec" {
+#    when    = create
+#    command = "ssh -o StrictHostKeychecking=no -i ${local_file.k3s_worker_gcp_key.filename} ubuntu@${google_compute_instance.k3s_worker_gcp.network_interface[0].access_config[0].nat_ip} \"sudo journalctl -u google-startup-scripts.service\""
+#  }
+#}
+
+resource "null_resource" "gcp_startup_script" {
+  depends_on = [null_resource.gcp_wait_for_status]
   provisioner "local-exec" {
     when    = create
-    command = "ssh -o StrictHostKeychecking=no -i ${local_file.k3s_master_key.filename} ubuntu@${aws_instance.k3s_master.public_ip} \"sudo journalctl -u google-startup-scripts.service\""
+    command = "ssh -o StrictHostKeychecking=no -i ${local_file.k3s_worker_gcp_key.filename} ubuntu@${google_compute_instance.k3s_worker_gcp.network_interface[0].access_config[0].nat_ip} \"curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k3s_master.public_ip}:6443 K3S_TOKEN=${chomp(data.local_file.k3s_token.content)} sh -s - --debug\""
   }
 }
 
